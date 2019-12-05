6668cbbda1e051e2fa39ff87ed10e778fccfea92
123dekart <mr.eshtemirov2303@gmail.com>
2019-12-05 16:59:29 +0500

    Registr-Product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'registr',
+    loadChildren: () => import('./pages/registr/registr.module').then( m => m.RegistrPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/pages/registr/registr.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registr',
+  templateUrl: './registr.page.html',
+  styleUrls: ['./registr.page.scss'],
+})
+export class RegistrPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
